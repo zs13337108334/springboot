c456e740e3c02c47545056c681e1fbe0061b854f
zs13337108334 <wb-zs878773@alibaba-inc.com>
2022-12-06 13:25:01 +0800

    切换密码

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        if(false)
+        {
+            System.out.println( "1");
+        } else if(true){
+            System.out.println("2");
+        }
+        System.out.println("3");
+
+    }
+}
